9c5c93f992409ba425ce0859891a7c273f992399
nilkevich <ilkevich.nastya@gmail.com>
2023-02-22 10:33:55 +0300

    new wolves

--- js/game_manager.js
+++ js/game_manager.js
@@ -126,6 +126,7 @@ GameManager.prototype.suspendGear = function () {
 GameManager.prototype.haltGear = function () {
   clearInterval(this.gameTimer);
   this.over = true;
+	
 };
 
 GameManager.prototype.upLevel = function () {
@@ -209,6 +210,7 @@ GameManager.prototype.runEgg = function (chicken) {
 GameManager.prototype.gameOver = function () {
   this.haltGear();
   this.HTMLredraw.gameOver();
+	
 };
 
 GameManager.prototype.gameWin = function () {
